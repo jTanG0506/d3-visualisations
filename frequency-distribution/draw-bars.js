783f15084f075745f1c48c868b3111bf640d5607
Jonathan Tang <hello@jtang.dev>
2020-03-23 22:36:02 +0000

    Draw mean line on frequency distribution

--- frequency-distribution/draw-bars.js
+++ frequency-distribution/draw-bars.js
@@ -64,7 +64,7 @@ async function drawBars() {
     .attr("y", d => yScale(yAccessor(d)))
     .attr("width", d => d3.max([0, xScale(d.x1) - xScale(d.x0) - barPadding]))
     .attr("height", d => dimensions.boundedHeight - yScale(yAccessor(d)))
-    .attr("fill", "cornflowerblue");
+    .attr("fill", "#50E3C2");
 
   const barText = binGroups.filter(yAccessor)
     .append("text")
@@ -86,7 +86,23 @@ async function drawBars() {
     .attr("y", dimensions.margin.bottom - 10)
     .attr("fill", "black")
     .style("font-size", "1.3em")
-    .text("Humidity")
+    .text("Humidity");
+
+  const mean = d3.mean(dataset, metricAccessor);
+  const meanLine = bounds.append("line")
+    .attr("x1", xScale(mean))
+    .attr("x2", xScale(mean))
+    .attr("y1", -15)
+    .attr("y2", dimensions.boundedHeight)
+    .attr("stroke", "#7433FF")
+    .attr("stroke-dasharray", "2px 4px");
+  const meanLabel = bounds.append("text")
+    .attr("x", xScale(mean))
+    .attr("y", -20)
+    .attr("fill", "#7433FF")
+    .style("font-size", "12px")
+    .style("text-anchor", "middle")
+    .text("mean");
 }
 
 drawBars();
