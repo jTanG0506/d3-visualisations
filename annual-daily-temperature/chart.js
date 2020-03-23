885230cf433e0c09361bca84615a6e5aee5e7d24
Jonathan Tang <hello@jtang.dev>
2020-03-23 02:19:17 +0000

    Create line plot for maximum temperature over last 365 days

---
+++ annual-daily-temperature/chart.js
@@ -0,0 +1,68 @@
+async function drawLineChart() {
+  const dataset = await d3.json("./weather_data.json");
+
+  const dateParser = d3.timeParse("%Y-%m-%d");
+  const yAccessor = d => d.temperatureMax;
+  const xAccessor = d => dateParser(d.date);
+
+  let dimensions = {
+    width: window.innerWidth * 0.9,
+    height: 400,
+    margin: {
+      top: 15,
+      right: 15,
+      bottom: 40,
+      left: 60,
+    },
+  };
+
+  let marginWidth = dimensions.margin.left + dimensions.margin.right;
+  let marginHeight = dimensions.margin.top + dimensions.margin.bottom;
+  dimensions.boundedWidth = dimensions.width - marginWidth;
+  dimensions.boundedHeight = dimensions.height - marginHeight;
+
+  const wrapper = d3.select("#wrapper")
+    .append("svg")
+    .attr("width", dimensions.width)
+    .attr("height", dimensions.height);
+
+  const bounds = wrapper.append("g")
+    .style("transform", `translate(${
+      dimensions.margin.left
+    }px, ${
+      dimensions.margin.top
+    }px)`);
+
+  console.log(d3.extent(dataset, yAccessor));
+
+  const yScale = d3.scaleLinear()
+    .domain(d3.extent(dataset, yAccessor))
+    .range([dimensions.boundedHeight, 0]);
+
+  const xScale = d3.scaleTime()
+    .domain(d3.extent(dataset, xAccessor))
+    .range([0, dimensions.boundedWidth]);
+
+  const lineGenerator = d3.line()
+    .x(d => xScale(xAccessor(d)))
+    .y(d => yScale(yAccessor(d)));
+
+  const line = bounds.append("path")
+    .attr("d", lineGenerator(dataset))
+    .attr("fill", "none")
+    .attr("stroke", "#af9358")
+    .attr("stroke-width", 2);
+
+  const yAxisGenerator = d3.axisLeft().scale(yScale);
+  const yAxis = bounds.append("g")
+    .call(yAxisGenerator);
+
+  const xAxisGenerator = d3.axisBottom().scale(xScale);
+  const xAxis = bounds.append("g")
+    .call(xAxisGenerator)
+    .style("transform", `translateY(${
+      dimensions.boundedHeight
+    }px)`);
+}
+
+drawLineChart();
